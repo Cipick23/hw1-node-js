267bec05a1831b320ae93b6cf758067faf54b500
CiprianR <133102658+Cipick23@users.noreply.github.com>
2024-04-29 21:54:16 +0300

    first commit

---
+++ contacts.js
@@ -0,0 +1,10 @@
+import fs from "node:fs/promises";
+import * as path from "node:path";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+console.log(__dirname);
+
+const contactsPath = "";
